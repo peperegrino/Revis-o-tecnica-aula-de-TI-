8bd720c5ebd238340ae8223f3879558b9ae31398
FELIPE <luizfalmeidalima38@gmail.com>
2026-02-23 18:01:37 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -41,47 +41,69 @@ public class Main {
 //                 break;
 //
 //        }
-        System.out.println("informe o primeiro numero:  ");
-        int num1 = sc.nextInt();
-        System.out.println("informe o segundo numero:   ");
-        int num2 = sc.nextInt();
 
-        System.out.println("informe a operação que voce quer:   1 - soma   2 - multiplicação   3 - divisão   4- subtração");
-        int operacoes = sc.nextInt();
 
-        switch (operacoes) {
-            case 1:
-                System.out.println("a soma dos numeros é: ");
-                if (num1 > num2) {
-                    System.out.println(num1 + num2);
-                } else {
-                    System.out.println(num2 + num1);
-                }
-                break;
-            case 2:
-                System.out.println("a multiplicação dos seus numeros é: " );
-                if (num1 > num2) {
-                    System.out.println(num1 * num2);
-                } else {
-                    System.out.println(num2 * num1);
-                }
-                break;
-            case 3:
-                System.out.println("a divisão dos seus numeros é: ");
-                if (num1 > num2) {
-                    System.out.println(num1 / num2);
-                } else {
-                    System.out.println(num2 / num1);
-                }
-                break;
-            case 4:
-                System.out.println("a subtração dos numeros é:");
-                if (num1 > num2) {
-                    System.out.println(num1 - num2);
-                } else {
-                    System.out.println(num2 - num1);
-                }
-                break;
+//        System.out.println("informe o primeiro numero:  ");
+//        int num1 = sc.nextInt();
+//        System.out.println("informe o segundo numero:   ");
+//        int num2 = sc.nextInt();
+//
+//        System.out.println("informe a operação que voce quer:   1 - soma   2 - multiplicação   3 - divisão   4- subtração");
+//        int operacoes = sc.nextInt();
+//
+//        switch (operacoes) {
+//            case 1:
+//                System.out.println("a soma dos numeros é: ");
+//                if (num1 > num2) {
+//                    System.out.println(num1 + num2);
+//                } else {
+//                    System.out.println(num2 + num1);
+//                }
+//                break;
+//            case 2:
+//                System.out.println("a multiplicação dos seus numeros é: " );
+//                if (num1 > num2) {
+//                    System.out.println(num1 * num2);
+//                } else {
+//                    System.out.println(num2 * num1);
+//                }
+//                break;
+//            case 3:
+//                System.out.println("a divisão dos seus numeros é: ");
+//                if (num1 > num2) {
+//                    System.out.println(num1 / num2);
+//                } else {
+//                    System.out.println(num2 / num1);
+//                }
+//                break;
+//            case 4:
+//                System.out.println("a subtração dos numeros é:");
+//                if (num1 > num2) {
+//                    System.out.println(num1 - num2);
+//                } else {
+//                    System.out.println(num2 - num1);
+//                }
+//
+//
+
+        System.out.println("indique a direção que voce quer ir: (w/a/s/d)");
+        String direcao = sc.next();
+
+        switch (direcao ) {
+            case "w":
+                System.out.println("o W anda para frente");
+            break;
+
+            case "a":
+                System.out.println("o A anda para a esquerda");
+            break;
+            case "s":
+                System.out.println("o S anda para trás");
+            break;
+            case "d":
+                System.out.println("o D anda para a esqueda");
         }
+
+
     }
 }
